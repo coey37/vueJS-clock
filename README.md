d1f64da1c529952a15a5fa457e59072746180ef4
coey37 <93464226+coey37@users.noreply.github.com>
2020-06-29 20:03:53 +0000

    Updated pengines

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Froogo
+A simple service to allow people to safely trade skins for PayPal money.
+
+## How does it work?
+A user will be chatting with someone they would like to trade with from a skin to PayPal. But you can't trust them to definitely send the money over PayPal. Both users will have an account, the user with the skin will select a skin they would like to trade. From this they can select an amount of points they want for this skin. They then send this deal to the receiving user. If the user accepts the deal it continues otherwise the deal is cancelled. Now this deal is in place for the next 24 hours. So how do we safely transport the skin avoiding a trade ban and guaranteeing money? Once the user trading the skin has sent the skin, they can click to check if the skin is in the receiving user's inventory and if it is the user will be rewarded with the points the user was willing to trade for. These points can be bought or paid out via PayPal.
---
+++ db/db.go
@@ -0,0 +1,99 @@
+
+package db
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/VolticFroogo/Froogo/db/dbCredentials"
+	"github.com/VolticFroogo/Froogo/helpers"
+	"github.com/VolticFroogo/Froogo/models"
+	_ "github.com/go-sql-driver/mysql" // Necessary for connecting to MySQL.
+)
+
+/*
+	Structs and variables
+*/
+
+var (
+	db *sql.DB
+)
+
+// InitDB initializes the Database.
+func InitDB() (err error) {
+	db, err = sql.Open(dbCredentials.Type, dbCredentials.ConnString)
+	if err != nil {
+		return
+	}
+
+	go jtiGarbageCollector()
+	return
+}
+
+/*
+	Helper functions
+*/
+
+func rowExists(query string, args ...interface{}) (exists bool, err error) {
+	query = fmt.Sprintf("SELECT exists (%s)", query)
+	err = db.QueryRow(query, args...).Scan(&exists)
+	return
+}
+
+/*
+	MySQL DataBase related functions
+*/
+
+// StoreRefreshToken generates, stores and then returns a JTI.
+func StoreRefreshToken() (jti models.JTI, err error) {
+	// No need to duplication check as the JTI takes input from time and are unique.
+	jti.JTI, err = helpers.GenerateRandomString(32)
+	if err != nil {
+		return
+	}
+
+	jti.Expiry = time.Now().Add(models.RefreshTokenValidTime).Unix()
+
+	_, err = db.Exec("INSERT INTO jti (jti, expiry) VALUES (?, ?)", jti.JTI, jti.Expiry)
+	if err != nil {
+		return
+	}
+
+	rows, err := db.Query("SELECT id FROM jti WHERE jti=? AND expiry=?", jti.JTI, jti.Expiry)
+	if err != nil {
+		return
+	}
+
+	defer rows.Close()
+
+	rows.Next()
+	err = rows.Scan(&jti.ID) // Scan data from query.
+	return
+}
+
+// GetJTI takes a JTI string and returns the JTI struct.
+func GetJTI(jti string) (jtiStruct models.JTI, err error) {
+	rows, err := db.Query("SELECT id, expiry FROM jti WHERE jti=?", jti)
+	if err != nil {
+		return
+	}
+
+	defer rows.Close()
+
+	jtiStruct.JTI = jti
+	rows.Next()
+	err = rows.Scan(&jtiStruct.ID, &jtiStruct.Expiry) // Scan data from query.
+	return
+}
+
+// CheckJTI returns the validity of a JTI.
+func CheckJTI(jti models.JTI) (valid bool, err error) {
+	if jti.Expiry > time.Now().Unix() { // Check if token has expired.
+		return true, nil // Token is valid.
+	}
+
+	_, err = db.Exec("DELETE FROM jti WHERE id=?", jti.ID)
+	if err != nil {
