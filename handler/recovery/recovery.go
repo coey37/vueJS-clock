e85c2648234c1c9488eada542ea9d73893f99e0f
coey37 <93464226+coey37@users.noreply.github.com>
2020-09-23 21:20:03 +0000

    send mail

--- handler/recovery/recovery.go
+++ handler/recovery/recovery.go
@@ -50,3 +50,79 @@ func Begin(w http.ResponseWriter, r *http.Request) {
 
 	if data.Captcha == "" {
 		helpers.JSONResponse(response{Code: Recaptcha}, w)
+		return // There is no captcha response.
+	}
+	captchaSuccess, err := captcha.Verify(data.Captcha, r.Header.Get("CF-Connecting-IP")) // Check the captcha.
+	if err != nil {
+		helpers.JSONResponse(response{Code: Recaptcha}, w)
+		helpers.ThrowErr(w, r, "Recaptcha error", err)
+		return
+	}
+	if !captchaSuccess {
+		helpers.JSONResponse(response{Code: Recaptcha}, w)
+		return // Unsuccessful captcha.
+	}
+
+	user, err := db.GetUserFromEmail(data.Email)
+	if err != nil {
+		helpers.JSONResponse(response{Code: Internal}, w)
+		helpers.ThrowErr(w, r, "Getting user error", err)
+		return
+	}
+	if user.UUID == 0 {
+		helpers.JSONResponse(response{Code: InvalidEmail}, w)
+		return
+	}
+
+	id := uid.New(64)
+
+	err = db.AddRecovery(id, user.UUID, data.Email)
+	if err != nil {
+		helpers.JSONResponse(response{Code: Internal}, w)
+		helpers.ThrowErr(w, r, "Adding recovery error", err)
+		return
+	}
+
+	err = SendEmail(id, data.Email)
+	if err != nil {
+		helpers.JSONResponse(response{Code: SendingEmail}, w)
+		helpers.ThrowErr(w, r, "Send email error", err)
+		return
+	}
+
+	helpers.JSONResponse(response{Code: Success}, w)
+}
+
+// SendEmail sends the recovery email.
+func SendEmail(id, email string) (err error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+	if err != nil {
+		return
+	}
+
+	// Create an SES session.
+	svc := ses.New(sess)
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(email),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String("To recover your password please click this link: <a href=\"https://froogo.co.uk/password-recovery?code=" + id + "\">recover password</a>."),
+				},
+				Text: &ses.Content{
+					Charset: aws.String("UTF-8"),
+					Data:    aws.String("To recover your password please click this link: https://froogo.co.uk/password-recovery?code=" + id),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String("UTF-8"),
