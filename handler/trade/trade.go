edbf202cfa6ecf0d9c79e14c7ece44bd2d03d3b3
coey37 <93464226+coey37@users.noreply.github.com>
2021-03-14 13:46:57 +0000

    fix code style.

--- handler/trade/trade.go
+++ handler/trade/trade.go
@@ -272,3 +272,95 @@ func Cancel(w http.ResponseWriter, r *http.Request) {
 		// Failed middleware (invalid credentials)
 		helpers.SuccessResponse(false, w, r)
 		return
+	}
+
+	uuidString := context.Get(r, "uuid").(string)
+	uuid, err := strconv.Atoi(uuidString)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error converting string to int", err)
+		return
+	}
+
+	user, err := db.GetUserFromID(uuid)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error getting user from ID", err)
+		return
+	}
+
+	offer, err := db.GetOffer(data.ID, user.UUID)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error getting offer", err)
+		return
+	}
+
+	if offer.UserUUID != user.UUID && offer.ReceiverUUID != user.UUID {
+		// This is exterior tampering
+		helpers.SuccessResponse(false, w, r)
+		return
+	}
+
+	err = db.DeleteOffer(data.ID)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error deleting offer", err)
+		return
+	}
+
+	helpers.SuccessResponse(true, w, r)
+}
+
+func Accept(w http.ResponseWriter, r *http.Request) {
+	var data response                            // Create struct to store data.
+	err := json.NewDecoder(r.Body).Decode(&data) // Decode response to struct.
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "JSON decoding error", err)
+		return
+	}
+
+	if !middleware.AJAX(w, r, models.AJAXData{CsrfSecret: data.CsrfSecret}) {
+		// Failed middleware (invalid credentials)
+		helpers.SuccessResponse(false, w, r)
+		return
+	}
+
+	uuidString := context.Get(r, "uuid").(string)
+	uuid, err := strconv.Atoi(uuidString)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error converting string to int", err)
+		return
+	}
+
+	user, err := db.GetUserFromID(uuid)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error getting user from ID", err)
+		return
+	}
+
+	offer, err := db.GetOffer(data.ID, user.UUID)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error getting offer", err)
+		return
+	}
+
+	if offer.ReceiverUUID != user.UUID {
+		// This is exterior tampering
+		helpers.SuccessResponse(false, w, r)
+		return
+	}
+
+	err = db.SetOfferStatus(models.OfferStatusAccepted, data.ID)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error setting offer status", err)
+		return
+	}
+
+	helpers.SuccessResponse(true, w, r)
+}
