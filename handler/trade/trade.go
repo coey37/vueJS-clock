e8f9bb9d8aa9fa0a55a5f6bc9e9c0263516b6d70
coey37 <93464226+coey37@users.noreply.github.com>
2021-03-14 08:14:47 +0000

    day 2

--- handler/trade/trade.go
+++ handler/trade/trade.go
@@ -162,3 +162,113 @@ func Offer(w http.ResponseWriter, r *http.Request) {
 			Success: false,
 			ID:      UserNotFound,
 		}, w)
+
+		return
+	}
+
+	data.ReceiverUUID = receiver.UUID
+
+	inventory, err := steam.GetInventory(user.SteamID)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error Getting Steam inventory", err)
+		return
+	}
+
+	item, assetid := GetItem(data.Classid, inventory)
+	data.Item = item
+
+	d := strings.TrimPrefix(data.Item.Actions[0].Link, "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S%owner_steamid%A%assetid%D")
+
+	data.FloatAPI, err = steam.FloatAPI(strconv.FormatInt(user.SteamID, 10), assetid, d)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Error using float API", err)
+		return
+	}
+
+	err = db.AddOffer(data)
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "Adding offer error", err)
+		return
+	}
+
+	helpers.SuccessResponse(true, w, r)
+}
+
+func Offers(w http.ResponseWriter, r *http.Request) {
+	uuidString := context.Get(r, "uuid").(string)
+
+	uuid, err := strconv.Atoi(uuidString)
+	if err != nil {
+		helpers.ThrowErr(w, r, "Error converting string to int", err)
+		return
+	}
+
+	user, err := db.GetUserFromID(uuid)
+	if err != nil {
+		helpers.ThrowErr(w, r, "Error getting user from ID", err)
+		return
+	}
+
+	t, err := template.ParseFiles("handler/templates/panel/trade-offers.html", "handler/templates/nested.html") // Parse the HTML pages
+	if err != nil {
+		helpers.ThrowErr(w, r, "Error template parsing", err)
+		return
+	}
+
+	csrfSecret, err := r.Cookie("csrfSecret")
+	if err != nil {
+		helpers.ThrowErr(w, r, "Error reading cookie", err)
+		return
+	}
+
+	offers, err := db.GetOffers(user.UUID)
+	if err != nil {
+		helpers.ThrowErr(w, r, "Error getting trade offers", err)
+		return
+	}
+
+	variables := models.TemplateVariables{
+		User:       user,
+		CsrfSecret: csrfSecret.Value,
+		Offers:     offers,
+	}
+	err = t.Execute(w, variables) // Execute temmplate with variables
+	if err != nil {
+		helpers.ThrowErr(w, r, "Template execution error", err)
+	}
+}
+
+func GetItem(classid string, inventory models.SteamInventory) (item models.Item, assetid string) {
+	for _, steamItem := range inventory.Descriptions {
+		if steamItem.Classid == classid {
+			item = steamItem
+			break
+		}
+	}
+
+	for _, asset := range inventory.Assets {
+		if asset.Classid == classid {
+			assetid = asset.Assetid
+			break
+		}
+	}
+
+	return
+}
+
+func Cancel(w http.ResponseWriter, r *http.Request) {
+	var data response                            // Create struct to store data.
+	err := json.NewDecoder(r.Body).Decode(&data) // Decode response to struct.
+	if err != nil {
+		helpers.SuccessResponse(false, w, r)
+		helpers.ThrowErr(w, r, "JSON decoding error", err)
+		return
+	}
+
+	if !middleware.AJAX(w, r, models.AJAXData{CsrfSecret: data.CsrfSecret}) {
+		// Failed middleware (invalid credentials)
+		helpers.SuccessResponse(false, w, r)
+		return
