45c312010637fd00f78c90c33266387a783583d3
coey37 <93464226+coey37@users.noreply.github.com>
2021-08-01 19:21:54 +0000

    Text edits

--- handler/users/self.go
+++ handler/users/self.go
@@ -123,4 +123,4 @@ func SendEmailVerification(user models.User, email string) (err error) {
 			Body: &ses.Body{
 				Html: &ses.Content{
 					Charset: aws.String("UTF-8"),
-					Data:    aws.String("To verify your email plea
+					Data:    aws.String("To verify your email please click this link: <a href=\"https://berniesbusybees.co.uk/verify-email/" + id + "\
