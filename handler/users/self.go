646ad4e981c8aab91eda99f1ad2489076f069254
coey37 <93464226+coey37@users.noreply.github.com>
2021-08-23 15:20:52 +0000

    GFS2: Fix incorrect invalidation for DIO/buffered I/O

--- handler/users/self.go
+++ handler/users/self.go
@@ -147,4 +147,8 @@ func SendEmailVerification(user models.User, email string) (err error) {
 // VerifyEmail is for verifying emails after a client clicks the verification URL.
 func VerifyEmail(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	code := v
+	code := vars["code"]
+
+	userUUID, email, err := db.GetEmailVerification(code)
+	if err != nil {
+		helper
