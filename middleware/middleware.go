1cd075c0da90bb6db2cde764a7097f56a6e19ba7
coey37 <93464226+coey37@users.noreply.github.com>
2022-05-08 08:26:50 +0000

    link correction

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -188,4 +188,5 @@ func WriteNewAuth(w http.ResponseWriter, r *http.Request, authTokenString, refre
 }
 
 // RedirectToLogin redirects the client to the login.
-func Redire
+func RedirectToLogin(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/login", http.St
