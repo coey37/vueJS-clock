807368e248230d1f5e5a2309201fdcccc9000ae6
coey37 <93464226+coey37@users.noreply.github.com>
2023-02-01 05:53:57 +0000

    fix samples

--- static/js/password-recovery.js
+++ static/js/password-recovery.js
@@ -59,4 +59,6 @@ $(document).ready(function(){
         for (i = 0; i < sURLVariables.length; i++) {
             sParameterName = sURLVariables[i].split('=');
     
-            if (sParameterName[0]
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+   
