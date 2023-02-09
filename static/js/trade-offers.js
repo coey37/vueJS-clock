1dcdf81a72f9587c9b8335b2e460d266f5cf6ba6
coey37 <93464226+coey37@users.noreply.github.com>
2023-02-09 17:17:05 +0000

    Fix #107

--- static/js/trade-offers.js
+++ static/js/trade-offers.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
         var offer = $(this).closest(".trade-offer");
         var id = parseInt(offer.attr("data-id"));
 
-        M.toast({html:
+        M.toast({html: "Cancelling trade offer."});
+
+        $.ajax({
+            url: "/panel/trade/cancel",
