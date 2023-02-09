9219982c56d8acca12ec2f76ce08da7be7b33e70
coey37 <93464226+coey37@users.noreply.github.com>
2023-02-09 11:26:53 +0000

    Tidy README

--- static/js/trade-offers.js
+++ static/js/trade-offers.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     var classid = $(location).attr("href").split('/').pop();
 
     $(".cancel-button").click(function(){
-        var offer = $(this).closest(".
+        var offer = $(this).closest(".trade-offer");
+        var id = parseInt(offer.attr("data-id"));
+
+        M.toast({html:
